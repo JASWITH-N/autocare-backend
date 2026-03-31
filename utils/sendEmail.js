44882dd15ff928d8cc02940f32046d137789d6f9
JASWITH-N <jaswithnimmala1811@gmail.com>
2026-04-01 00:18:30 +0530

    Switched to Brevo SMTP

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -3,16 +3,13 @@ import nodemailer from 'nodemailer';
 const sendEmail = async (options) => {
   try {
     const transporter = nodemailer.createTransport({
-      host: 'smtp.gmail.com',
+      host: 'smtp-relay.brevo.com',
       port: 587,
       secure: false,
       auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
       },
-      connectionTimeout: 10000,
-      greetingTimeout: 10000,
-      socketTimeout: 10000,
     });
 
     const mailOptions = {
